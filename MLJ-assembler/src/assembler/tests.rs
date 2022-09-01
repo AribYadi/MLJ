e093625f3d15b127d8caf30c598aaa603c4d73f3
AribYadi <aribyadi@gmail.com>
2022-09-01 10:57:29 +0700

    Write unit tests for assembler

--- MLJ-assembler/src/assembler/tests.rs
+++ MLJ-assembler/src/assembler/tests.rs
@@ -20,3 +20,34 @@ fn test_assemble(input: &str, expect: &[u16]) {
 
 #[test]
 fn test_assemble_EXT() { test_assemble("ext", &[0x0000]) }
+
+#[test]
+fn test_assemble_STR() { test_assemble("str rpc 123", &[0x0BFB]) }
+
+#[test]
+fn test_assemble_LDR() { test_assemble("ldr rpc 0x7B", &[0x13FB]) }
+
+#[test]
+fn test_assemble_INC() {
+  test_assemble("inc 0xFF", &[0x18FF]);
+  test_assemble("inc rsp", &[0x1C08]);
+}
+
+#[test]
+fn test_assemble_DEC() {
+  test_assemble("dec 0xFF", &[0x20FF]);
+  test_assemble("dec rsp", &[0x2408]);
+}
+
+#[test]
+fn test_assemble_CMP() {
+  test_assemble("cmp r0 r1 eq", &[0x2801]);
+  test_assemble("cmp r0 r1 ne", &[0x2901]);
+  test_assemble("cmp r0 r1 lt", &[0x2A01]);
+  test_assemble("cmp r0 r1 le", &[0x2B01]);
+  test_assemble("cmp r0 r1 gt", &[0x2C01]);
+  test_assemble("cmp r0 r1 ge", &[0x2D01]);
+}
+
+#[test]
+fn test_assemble_JMC() { test_assemble("jmc 0x0201", &[0x3201]) }
